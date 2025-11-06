fe27f6a46866be5f1462163f78ab377f91f6e28c
see1004 <see1004@alu.ubu.es>
2025-11-06 15:20:24 +0100

    Cambios menores, prueba git

--- Base_Code.R
+++ Base_Code.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(rjson)
+library(tidyjson)
 
 data_nef_url <- "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/hlth_co_ren?format=JSON&unit=NR&unit=P_HTHAB&icd9cm=CM3995&icd9cm=CM3995_556&icd9cm=CM556&lang=EN"
 datos_nefro <- fromJSON(file = data_nef_url)
 
-view(datos_nefro)
+datos_nefro_tab <- spread_all(datos_nefro)
+head(datos_nefro)
 str(datos_nefro)                        
